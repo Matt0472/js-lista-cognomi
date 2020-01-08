1391e929b6d43476a9912a7106ad859c62bf9ee6
Mattia Pedone <mattiapedone93@gmail.com>
2020-01-08 16:01:32 +0100

    creazione prime variabili e array

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,14 @@
-alert('ciao');
+// Chiedi all’utente il cognome,
+// inseriscilo in un array con altri cognomi,
+// stampa la lista ordinata alfabeticamente (in html ul > li).
+// Scrivi anche la posizione della lista in cui il nuovo utente si trova.
+
+var userName = prompt('Qual è il tuo cognome?');
+console.log(userName);
+
+var familyNameGroup = ['Nesta', 'Rui costa', 'Shevchenko', 'Baresi', 'Maldini', 'Ancelotti', 'Inzaghi', 'Gattuso'];
+console.log(familyNameGroup);
+
+
+familyNameGroup.push(userName);
+console.log(familyNameGroup);
